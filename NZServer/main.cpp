d868330f59063e44313b058bb8ebb52b895dc565
Maximilian Maldacker <mmaldacker@Maximilians-MacBook-Pro.local>
2013-12-25 21:16:56 +0100

    Initial Commit

---
+++ NZServer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NZServer
+//
+//  Created by Maximilian Maldacker on 25/12/2013.
+//  Copyright (c) 2013 Maximilian Maldacker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
